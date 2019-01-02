731ff5e3b2c569403b1dc54496415e130d9bde92
K4J4N <kajan.14@cse.mrt.ac.lk>
2019-01-02 16:14:32 +0530

    dynamic CSS styling

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 new Vue({
   el: '#app',
   data: {
-    counter: 0,
-    secondCounter: 0
-  },
-  watch: {
-    counter: function (value) {
-      var vm = this;
-      setTimeout(function () {
-        vm.counter = 0;
-      }, 2000)
-    }
-  },
-  computed: {
-    output: function () {
-      console.log('Computed');
-      return this.counter > 5 ? 'Greater than 5' : 'Equal or smaller than 5';
-    }
+    attachRed: false
   },
   methods: {
     result: function () {
