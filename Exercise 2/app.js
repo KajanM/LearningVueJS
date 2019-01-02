154450e5b1497c033d8d8c757747d193a94d8d1f
K4J4N <kajan.14@cse.mrt.ac.lk>
2019-01-02 13:20:45 +0530

    solve exercise 2

--- Exercise 2/app.js
+++ Exercise 2/app.js
@@ -2,5 +2,13 @@ new Vue({
   el: '#exercise',
   data: {
     value: ''
+  },
+  methods: {
+    alert: function() {
+      alert('Alert!');
+    },
+    onKeyDown: function(event) {
+      this.value = event.target.value;
+    }
   }
 });
