4f51dc1ab7859c00c7209a4a2dda0630335c17a4
K4J4N <kajan.14@cse.mrt.ac.lk>
2019-01-02 15:45:56 +0530

    solve exercise 3

--- Exercise 3/app.js
+++ Exercise 3/app.js
@@ -1,6 +1,19 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
-        }
-    });
+  el: '#exercise',
+  data: {
+    value: 0
+  },
+  computed: {
+    result: function() {
+      return this.value != 37 ? 'not there yet' : 'done';
+    }
+  },
+  watch: {
+    value: function () {
+      var vm = this;
+      setTimeout(function () {
+        vm.value = 0;
+      }, 5000);
+    }
+  }
+});
